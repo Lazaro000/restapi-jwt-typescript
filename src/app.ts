162e3df03d883de165fff5403d28fc35d2aa4b22
Lázaro <lazaroyaesta@gmail.com>
2022-07-18 19:46:30 +0200

    ✨ Initialize server

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+/**
+ * * Initializations
+ */
+const app = express();
+
+/**
+ * * Settings
+ */
+app.set("port", process.env.PORT || 3000);
+
+/**
+ * * Middlewares
+ */
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/**
+ * Routes
+ */
+app.get("/", (req, res) => {
+  res.send(`The API is at http://localhost:${app.get("port")}`);
+});
+
+export default app;
