62d15b6a8a1b86d53398b5fd8dc1b4ec07a7d82c
Lázaro <lazaroyaesta@gmail.com>
2022-07-19 16:00:19 +0200

    👔 Add User Controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,44 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+import User, { IUser } from "../models/User";
 
-export const signUp = (req: Request, res: Response) => {
-  res.send("signup");
+const createToken = (user: IUser) => {
+  jwt.sign({ id: user.id, email: user.email }, config.jwtSecretKey, {
+    expiresIn: 864000,
+  });
 };
 
-export const signIn = (req: Request, res: Response) => {
-  res.send("signin");
+export const signUp = async (req: Request, res: Response) => {
+  if (!req.body.email || !req.body.password)
+    return res
+      .status(400)
+      .json({ message: "Please, send your email and password" });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).json({ message: "The user already exists" });
+
+  const newUser = new User(req.body);
+  await newUser.save();
+
+  return res.status(201).json(newUser);
+};
+
+export const signIn = async (req: Request, res: Response) => {
+  if (!req.body.email || !req.body.password)
+    return res
+      .status(400)
+      .json({ message: "Please, send your email and password" });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return res.status(400).json({ message: "The user does not exist" });
+
+  const isMatch = await user.comparePassword(req.body.password);
+
+  if (isMatch) return res.status(200).json({ token: createToken(user) });
+
+  return res
+    .status(400)
+    .json({ message: "The email or password are incorrect" });
 };
